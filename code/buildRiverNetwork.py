9efa728708bb1790ef1ceb4b8cbbcbf999b6812b
Hishiv Shah <shahh14@cardiff.ac.uk>
2015-03-04 16:05:23 +0000

    Created two new scripts. readOSMeridian.py loads coastline, rivers, and welsh national boundary datasets into spatialite. buildRiverNetwork.py creates a river network.

---
+++ code/buildRiverNetwork.py
@@ -0,0 +1,142 @@
+import sqlite3
+
+
+# Database path
+sqliteDb = "../results/2015-03-04.sqlite"
+
+# Connect to database
+with sqlite3.connect(sqliteDb) as db:
+    db.enable_load_extension(True)
+    db.load_extension("spatialite")
+    cur = db.cursor()
+    cur.execute("SELECT InitSpatialMetaData();")
+       
+    # Create table for river edges
+    cur.execute("DROP TABLE IF EXISTS riverEdges;")
+    cur.execute("""CREATE TABLE riverEdges (
+                   id TEXT PRIMARY KEY,
+                   code INTEGER,
+                   startNodeId INTEGER,
+                   endNodeId INTEGER);""")
+    cur.execute("""SELECT AddGeometryColumn('riverEdges',
+                                            'geometry',
+                                            27700,
+                                            'LINESTRING');""")
+    cur.execute("""WITH RECURSIVE
+                   results(identifier, code, geometry) AS
+                   (SELECT r.identifier, r.code, r.geometry
+                   FROM osRivers r, wales w
+                   WHERE ST_INTERSECTS(r.geometry, w.geometry)
+                   AND r.ROWID IN 
+                   (SELECT ROWID
+                   FROM SpatialIndex
+                   WHERE f_table_name = 'osRivers'
+                   AND search_frame = w.geometry)
+                   UNION
+                   SELECT r.identifier, r.code, r.geometry
+                   FROM osRivers r, results s
+                   WHERE ST_INTERSECTS(r.geometry, s.geometry)
+                   AND r.ROWID IN 
+                   (SELECT ROWID
+                   FROM SpatialIndex
+                   WHERE f_table_name = 'osRivers'
+                   AND search_frame = s.geometry))
+                   INSERT INTO riverEdges (id, code, geometry)
+                   SELECT identifier, code, ST_LineMerge(ST_Union(geometry))
+                   FROM results
+                   GROUP BY identifier, code;""")
+    cur.execute("SELECT CreateSpatialIndex('riverEdges', 'geometry');")
+    
+    # Create table for river nodes
+    cur.execute("DROP TABLE IF EXISTS riverNodes;")
+    cur.execute("""CREATE TABLE riverNodes (
+                   id INTEGER PRIMARY KEY);""")
+    cur.execute("""SELECT AddGeometryColumn('riverNodes',
+                                            'geometry',
+                                            27700,
+                                            'POINT');""")
+
+    # Create nodes where coastline intersects rivers
+    cur.execute("""INSERT INTO riverNodes (geometry)
+                   SELECT ST_Intersection(e.geometry, c.geometry)
+                   FROM riverEdges e, osCoastline c
+                   WHERE ST_Intersects(e.geometry, c.geometry)
+                   AND c.ROWID IN 
+                   (SELECT ROWID
+                   FROM SpatialIndex
+                   WHERE f_table_name = 'osCoastline'
+                   AND search_frame = e.geometry)
+                   GROUP BY e.geometry;""")
+    cur.execute("SELECT CreateSpatialIndex('riverNodes', 'geometry');")
+                   
+
+    while cur.execute("""SELECT count(e.id)
+                         FROM riverEdges e, riverNodes n
+                         WHERE e.endNodeId IS NULL
+                         AND ST_Touches(e.geometry, n.geometry)
+                         AND e.ROWID IN 
+                         (SELECT ROWID
+                         FROM SpatialIndex
+                         WHERE f_table_name = 'riverEdges'
+                         AND search_frame = n.geometry);""").fetchone()[0] > 0:
+                         
+        print cur.execute("""SELECT count(e.id)
+                         FROM riverEdges e, riverNodes n
+                         WHERE e.endNodeId IS NULL
+                         AND ST_Touches(e.geometry, n.geometry)
+                         AND e.ROWID IN 
+                         (SELECT ROWID
+                         FROM SpatialIndex
+                         WHERE f_table_name = 'riverEdges'
+                         AND search_frame = n.geometry);""").fetchone()[0]
+                   
+        # Reverse incorrectly orientated linestrings
+        cur.execute("""UPDATE riverEdges
+                       SET geometry = ST_Reverse(geometry)
+                       WHERE id IN
+                       (SELECT e.id
+                       FROM riverEdges e, riverNodes n
+                       WHERE e.endNodeId IS NULL
+                       AND ST_Intersects(n.geometry,
+                                         ST_StartPoint(e.geometry))
+                       AND n.ROWID IN 
+                       (SELECT ROWID
+                       FROM SpatialIndex
+                       WHERE f_table_name = 'riverNodes'
+                       AND search_frame = e.geometry));""")
+                       
+        # Set riverEdge endNodeId attribute
+        cur.execute("""UPDATE riverEdges
+                       SET endNodeId = (SELECT n.id
+                       FROM riverNodes n
+                       WHERE ST_Intersects(ST_EndPoint(riverEdges.geometry), n.geometry)
+                       AND n.ROWID IN 
+                       (SELECT ROWID
+                       FROM SpatialIndex
+                       WHERE f_table_name = 'riverNodes'
+                       AND search_frame = riverEdges.geometry))
+                       WHERE endNodeId IS NULL;""")
+                       
+        # Create nodes at first vertex of riverEdge
+        cur.execute("""INSERT INTO riverNodes (geometry)
+                       SELECT ST_StartPoint(e.geometry)
+                       FROM riverEdges e
+                       WHERE e.startNodeId IS NULL
+                       AND e.endNodeId IS NOT NULL;""")
+                       
+        # Set riverEdge startNodeId attribute
+        cur.execute("""UPDATE riverEdges
+                       SET startNodeId = (SELECT n.id
+                       FROM riverNodes n
+                       WHERE ST_Intersects(ST_StartPoint(riverEdges.geometry), n.geometry)
+                       AND n.ROWID IN 
+                       (SELECT ROWID
+                       FROM SpatialIndex
+                       WHERE f_table_name = 'riverNodes'
+                       AND search_frame = riverEdges.geometry))
+                       WHERE startNodeId IS NULL
+                       AND endNodeId IS NOT NULL;""")
+        
+   
+    # Commit changes
+    db.commit()
---
+++ code/readOSMeridian2.py
@@ -0,0 +1,139 @@
+import sqlite3
+import os
+
+import ogr
+
+# Inputs
+osMeridian2Dir = "../data/2015-02-27/meridian2_national_841398"
+districtsShp = os.path.join(osMeridian2Dir, "district_region.shp")
+riversShp = os.path.join(osMeridian2Dir, "river_polyline.shp")
+coastShp = os.path.join(osMeridian2Dir, "coast_ln_polyline.shp")
+
+# Database path
+sqliteDb = "../results/2015-03-04.sqlite"
+
+# Welsh district names
+districtNames = ['GWYNEDD - GWYNEDD',
+                 'SIR Y FFLINT - FLINTSHIRE',
+                 'CASNEWYDD - NEWPORT',
+                 'SIR YNYS MON - ISLE OF ANGLESEY',
+                 'MERTHYR TUDFUL - MERTHYR TYDFIL',
+                 'SIR BENFRO - PEMBROKESHIRE',
+                 'BRO MORGANNWG - THE VALE OF GLAMORGAN',
+                 'WRECSAM - WREXHAM',
+                 'TOR-FAEN - TORFAEN',
+                 'CASTELL-NEDD PORT TALBOT - NEATH PORT TALBOT',
+                 'SIR FYNWY - MONMOUTHSHIRE',
+                 'CAERFFILI - CAERPHILLY',
+                 'PEN-Y-BONT AR OGWR - BRIDGEND',
+                 'BLAENAU GWENT - BLAENAU GWENT',
+                 'POWYS - POWYS',
+                 'CONWY - CONWY',
+                 'SIR DDINBYCH - DENBIGHSHIRE',
+                 'RHONDDA CYNON TAF - RHONDDA CYNON TAF',
+                 'SIR GAERFYRDDIN - CARMARTHENSHIRE',
+                 'SIR CEREDIGION - CEREDIGION',
+                 'ABERTAWE - SWANSEA',
+                 'CAERDYDD - CARDIFF']
+
+# Connect to database
+with sqlite3.connect(sqliteDb) as db:
+    db.enable_load_extension(True)
+    db.load_extension("spatialite")
+    cur = db.cursor()
+    cur.execute("SELECT InitSpatialMetaData(1);")
+    
+    # Create districts table
+    cur.execute("DROP TABLE IF EXISTS osDistricts;")
+    cur.execute("""CREATE TABLE osDistricts (
+                   id INTEGER PRIMARY KEY AUTOINCREMENT,
+                   name TEXT);""")
+    cur.execute("""SELECT AddGeometryColumn('osDistricts',
+                                            'geometry',
+                                            27700,
+                                            'POLYGON');""")
+
+    # Create rivers table
+    cur.execute("DROP TABLE IF EXISTS osRivers;")
+    cur.execute("""CREATE TABLE osRivers (
+                   id INTEGER PRIMARY KEY AUTOINCREMENT,
+                   identifier TEXT,
+                   code INTEGER,
+                   name TEXT);""")
+    cur.execute("""SELECT AddGeometryColumn('osRivers',
+                                            'geometry',
+                                            27700,
+                                            'LINESTRING');""")
+                                            
+    # Create coastline table
+    cur.execute("DROP TABLE IF EXISTS osCoastline;")
+    cur.execute("""CREATE TABLE osCoastline (
+                   id INTEGER PRIMARY KEY AUTOINCREMENT);""")
+    cur.execute("""SELECT AddGeometryColumn('osCoastline',
+                                            'geometry',
+                                            27700,
+                                            'LINESTRING');""")                            
+                                            
+    # Read districts from shape
+    dataSource = ogr.Open(districtsShp)
+    layer = dataSource.GetLayer()
+    layer.SetAttributeFilter("NAME IN ('%s')" % "','".join(districtNames))
+    
+    for feature in layer:
+        geometry = feature.GetGeometryRef().ExportToWkt()
+        name = feature.GetField("NAME")
+        
+        # Insert data into osDistricts table
+        cur.execute("""INSERT INTO osDistricts (name, geometry)
+                       VALUES (?, ST_GeomFromText(?, 27700));""",
+                    (name, geometry))
+
+    # Read rivers from shape
+    dataSource = ogr.Open(riversShp)
+    layer = dataSource.GetLayer()
+    layer.SetAttributeFilter("CODE IN (6223, 6224, 6225, 6230, 6232)")
+    
+    for feature in layer:
+        geometry = feature.GetGeometryRef().ExportToWkt()
+        identifier = feature.GetField("IDENTIFIER")
+        code = feature.GetField("CODE")
+        name = feature.GetField("NAME")
+        
+        # Insert data into osRivers table
+        cur.execute("""INSERT INTO osRivers (identifier, code, name, geometry)
+                       VALUES (?, ?, ?, ST_GeomFromText(?, 27700));""",
+                    (identifier, code, name, geometry))
+                    
+    # Read coastlines from shape
+    dataSource = ogr.Open(coastShp)
+    layer = dataSource.GetLayer()
+    
+    for feature in layer:
+        geometry = feature.GetGeometryRef().ExportToWkt()
+        
+        # Insert data into osCoastline table
+        cur.execute("""INSERT INTO osCoastline (geometry)
+                       VALUES (ST_GeomFromText(?, 27700));""",
+                    (geometry,))
+                    
+    # Build spatial indexes
+    cur.execute("SELECT CreateSpatialIndex('osDistricts', 'geometry');")
+    cur.execute("SELECT CreateSpatialIndex('osRivers', 'geometry');")
+    cur.execute("SELECT CreateSpatialIndex('osCoastline', 'geometry');")
+    
+    # Dissolve welsh districts to single polygon
+    cur.execute("DROP TABLE IF EXISTS wales;")
+    cur.execute("""CREATE TABLE wales (
+                   id INTEGER PRIMARY KEY AUTOINCREMENT,
+                   name TEXT);""")
+    cur.execute("""SELECT AddGeometryColumn('Wales',
+                                            'geometry',
+                                            27700,
+                                            'MULTIPOLYGON');""")
+    cur.execute("""INSERT INTO wales (name, geometry)
+                   SELECT 'Wales', ST_UNION(geometry)
+                   FROM osDistricts;""")
+    cur.execute("SELECT CreateSpatialIndex('wales', 'geometry');")    
+  
+    # Commit changes to database
+    db.commit()
